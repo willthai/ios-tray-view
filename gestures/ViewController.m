858ad96220979373b0b1af9052a5c9802e6d428a
William Thai <wthai1027@gmail.com>
2017-02-01 11:32:19 -0800

    draggable tray completed

--- gestures/ViewController.m
+++ gestures/ViewController.m
@@ -9,10 +9,27 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *trayView;
+@property (nonatomic, assign) CGPoint trayOrigCenter;
 @end
 
 @implementation ViewController
+- (IBAction)onTrayPanGesture:(UIPanGestureRecognizer *)sender {
+    // Absolute (x,y) coordinates in parentView
+    CGPoint location = [sender locationInView:self.view];
+    
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        NSLog(@"Gesture began at: %@", NSStringFromCGPoint(location));
+        self.trayOrigCenter = [self.trayView center];
+    } else if (sender.state == UIGestureRecognizerStateChanged) {
+        NSLog(@"Gesture changed at: %@", NSStringFromCGPoint(location));
+        CGPoint translation = [sender translationInView:self.trayView];
+
+        self.trayView.center = CGPointMake(self.trayOrigCenter.x, self.trayOrigCenter.y + translation.y);
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+        NSLog(@"Gesture ended at: %@", NSStringFromCGPoint(location));
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
